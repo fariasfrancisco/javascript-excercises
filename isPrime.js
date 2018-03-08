76ba9cf89b9f7fdcce76c472886d7f8d933661fb
Francisco Farias <fariasfrancisco@users.noreply.github.com>
2018-03-08 01:53:32 -0300

    Update isPrime.js

--- isPrime.js
+++ isPrime.js
@@ -3,22 +3,23 @@
  * @param n
  */
 
-const isPrime = (n) => {
-  if (!Number.isInteger(n)) return console.log(n + ' is not an Integer');
+function isPrime (n) {
+  if (!Number.isInteger(n)) return console.log(`${n} is not an Integer`)
 
-  const sqrt = Math.round(Math.sqrt(n));
-  let isPrime = true;
+  const sqrt = Math.round(Math.sqrt(n))
+  let isPrime = true
 
   if (n !== 2 && n % 2 === 0 || n < 2) {
-    isPrime = false;
+    isPrime = false
   } else {
     for (let i = 3; i < sqrt; i += 2) {
       if (n % i === 0) {
-        isPrime = false;
-        break;
+        isPrime = false
+        
+        break
       }
     }
   }
 
-  console.log('is ' + n + ' prime? ' + isPrime);
-};
+  console.log(`is ${n} prime? ${isPrime : 'yes' : 'no'}`)
+}
