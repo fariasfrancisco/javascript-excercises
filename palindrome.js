ea235bdcf9eb1b015dea423d543c66f4d2cbe00e
Francisco Farias <fariasfrancisco@users.noreply.github.com>
2018-03-08 01:55:45 -0300

    Update palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -3,19 +3,20 @@
  * @param word
  */
 
-const palindrome = (word) => {
-  if (typeof word !== 'string') return console.log(word + ' is not a string');
+function palindrome (word) {
+  if (typeof word !== 'string') return console.log(`${word} is not a string`)
 
-  const middle = word.length % 2 === 0 ? word.length / 2 : (word.length + 1) / 2;
-  let isPalindrome = true,
-    j = word.length - 1;
+  const middle = word.length % 2 === 0 ? word.length / 2 : (word.length + 1) / 2
+  let isPalindrome = true
+  let j = word.length - 1
 
   for (let i = 0; i < middle; i++) {
     if (word.charAt(i) !== word.charAt(j - i)) {
-      isPalindrome = false;
-      break;
+      isPalindrome = false
+
+      break
     }
   }
 
-  console.log('is ' + word + ' a palindrome? ' + isPalindrome);
-};
+  console.log(`is ${word} a palindrome? ${isPalindrome ? 'yes' : 'no'}`)
+}
