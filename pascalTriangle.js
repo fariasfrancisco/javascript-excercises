481fc12ed00aaa693f2b556481ea1f0b55428c6b
Francisco Farias <fariasfrancisco@users.noreply.github.com>
2018-03-08 01:59:24 -0300

    Update pascalTriangle.js

--- pascalTriangle.js
+++ pascalTriangle.js
@@ -3,40 +3,45 @@
  * @param n
  */
 
-const pascalTriangle = (n) => {
-  if (!Number.isInteger(n)) return console.log(n + ' is not an Integer');
+function pascalTriangle (n) {
+  if (!Number.isInteger(n)) return console.log(`${n} is not an Integer`)
 
-  let lines = [];
-
+  const lines = []
+  let value
+  let size
+  let arr
+  
   for (let i = 0; i < n; i++) {
-    let size = i + 1,
-      arr = [1];
+    size = i + 1
+    arr = [1]
 
     if (i === 0) {
-      lines.push(arr);
+      lines.push(arr)
+      
       continue;
     } else {
       if (i === 1) {
-        arr.push(1);
-        lines.push(arr);
-        continue;
+        arr.push(1)
+        lines.push(arr)
+        
+        continue
       }
     }
 
+    
+    
     for (let j = 1; j < size; j++) {
       if (j + 1 < size) {
-        const value = lines[i - 1][j - 1] + lines[i - 1][j];
+        value = lines[i - 1][j - 1] + lines[i - 1][j]
 
         arr.push(value)
       } else {
-        arr.push(1);
+        arr.push(1)
       }
     }
 
-    lines.push(arr);
+    lines.push(arr)
   }
 
-  lines.forEach(line => {
-    console.log(line);
-  });
-};
+  lines.forEach(console.log)
+}
